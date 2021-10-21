d7c91b9d9b06f5b56f74df5c81aaaf5102ea543e
MarcosIsaias <contato.marcosisaias@gmail.com>
2021-10-20 21:46:42 -0300

    Projeto JAVAWEB

---
+++ web/src/main/java/controller/ViewController.java
@@ -0,0 +1,22 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ViewController {
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping("/ cadastro")
+    public String cadastro() {
+        return "cadastro";
+    }
+
+    @RequestMapping("/ home")
+    public String home() {
+        return "personagem";
+    }
+}
